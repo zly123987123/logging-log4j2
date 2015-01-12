36a5bb15b726a9ed3babfee2f8273d3c52d206c9
rpopma <rpopma@apache.org>
2015-01-12 17:40:13 +0900

    added FormattingInfo and Charset fields, provide convenience methods for subclasses including LRU caching functionality

--- log4j-core/src/main/java/org/apache/logging/log4j/core/pattern/AbstractPatternConverter.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/pattern/AbstractPatternConverter.java
@@ -16,9 +16,12 @@
  */
 package org.apache.logging.log4j.core.pattern;
 
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.status.StatusLogger;
+import java.nio.charset.Charset;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.util.Charsets;
+import org.apache.logging.log4j.core.util.ConcurrentLRUCache;
+import org.apache.logging.log4j.status.StatusLogger;
 
 /**
  * AbstractPatternConverter is an abstract class that provides the formatting functionality that derived classes need.
@@ -43,15 +46,29 @@ public abstract class AbstractPatternConverter implements PatternConverter {
      */
     private final String style;
 
+    private final FormattingInfo formattingInfo;
+    protected final boolean hasFormattingInfo;    
+    protected Charset charset;
+    private volatile ConcurrentLRUCache<Object, String> textCache;
+    private volatile ConcurrentLRUCache<Object, byte[]> binaryCache;
+    protected ThreadLocal<StringBuilder> formattingBuffer = new ThreadLocal<StringBuilder>() {
+        @Override
+        protected StringBuilder initialValue() {
+            return new StringBuilder(128);
+        };
+    };
+
     /**
      * Create a new pattern converter.
      *
-     * @param name  name for pattern converter.
+     * @param name name for pattern converter.
      * @param style CSS style for formatted output.
      */
-    protected AbstractPatternConverter(final String name, final String style) {
+    protected AbstractPatternConverter(final String name, final String style, final FormattingInfo formattingInfo) {
         this.name = name;
         this.style = style;
+        this.formattingInfo = formattingInfo;
+        this.hasFormattingInfo = formattingInfo != null && formattingInfo != FormattingInfo.getDefault();
     }
 
     /**
@@ -74,12 +91,106 @@ public abstract class AbstractPatternConverter implements PatternConverter {
      * This information is currently used only by HtmlLayout.
      * </p>
      *
-     * @param e
-     *        null values are accepted
+     * @param e null values are accepted
      * @return the name of the conversion pattern
      */
     @Override
     public String getStyleClass(final Object e) {
         return style;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public FormattingInfo getFormattingInfo() {
+        return formattingInfo;
+    }
+    
+    protected String applyFormattingInfo(final String unformatted) {
+        if (!hasFormattingInfo) {
+            return unformatted;
+        }
+        final FormattingInfo info = formattingInfo;
+        final StringBuilder buffer = formattingBuffer.get();
+        buffer.setLength(0);
+        buffer.append(unformatted);
+        info.format(0, buffer);
+        return buffer.toString();
+    }
+    
+    protected String getCachedFormattedString(final String unformatted) {
+        if (!hasFormattingInfo) {
+            return unformatted; // no need to even access the cache
+        }
+        return getCachedFormattedString((Object) unformatted);
+    }
+    
+    protected String getCachedFormattedString(final Object object) {        
+        // first see if we already have the formatted value cached
+        final ConcurrentLRUCache<Object, String> cache = getTextCache();
+        final String cachedResult = cache.get(object);
+        if (cachedResult != null) {
+            return cachedResult; // if we have it cached we're done
+        }
+        // if not in cache we need to create the formatted String
+        final String formatted = applyFormattingInfo(convert(object));
+        cache.set(object, formatted); // cache the result for next time
+        return formatted;
+    }
+    
+    protected String convert(final Object object) {
+        return object.toString();
+    }
+    
+    protected byte[] getCachedFormattedBytes(final Object unformatted, final Charset charset) {
+        // first see if we already have the formatted value cached
+        final ConcurrentLRUCache<Object, byte[]> cache = getBinaryCache();
+        final byte[] cachedResult = cache.get(unformatted);
+        if (cachedResult != null) {
+            return cachedResult;
+        }
+        // if not in cache we need to create the formatted String and get its bytes
+        final String formatted = applyFormattingInfo(convert(unformatted));
+        final byte[] result = Charsets.getBytes(formatted, charset);
+        cache.set(unformatted, result);
+        return result;
+    }
+
+    protected ConcurrentLRUCache<Object, String> getTextCache() {
+        if (textCache == null) {
+            textCache = new ConcurrentLRUCache<Object, String>(128);
+        }
+        return textCache;
+    }
+
+    protected ConcurrentLRUCache<Object, byte[]> getBinaryCache() {
+        if (binaryCache == null) {
+            binaryCache = new ConcurrentLRUCache<Object, byte[]>(128);
+        }
+        return binaryCache;
+    }
+    
+    @Override
+    public String toString() {
+        final StringBuilder result = new StringBuilder(256);
+        result.append(getClass().getSimpleName()).append('[');
+        result.append(getName());
+        if (style != null) {
+            result.append(", style=").append(style);
+        }
+        if (formattingInfo != null) {
+            result.append(", ").append(formattingInfo);
+        }
+        result.append(']');
+        return result.toString();
+    }
+
+    public Charset getCharset() {
+        return charset;
+    }
+
+    public void setCharset(Charset charset) {
+        this.charset = charset;
     }
 }
